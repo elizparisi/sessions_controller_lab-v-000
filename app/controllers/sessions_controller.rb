4ae5c921f1b00e812339566f8eff89e76648fb74
Liz P <parisielizabeth@gmail.com>
2019-12-29 20:47:17 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
   
     if params[:name] == nil || params[:name].empty?
-      redirect_to '/login'
+      redirect_to '/new'
       
     else 
       session[:name] = params[:name]
